b042e1b714394cf89bc9576e0a551b585b03282f
Kevinelorigi <kevinelorigi2002@gmail.com>
2022-08-11 14:49:12 -0500

    segundo

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import React, { useState, useRef, useEffect } from "react";
+import Message from "./Message";
+import SendMessages from "./SendMessages";
 
 const style = {
-    main: `flex flex-col p-[10px] relati`
-}
+  main: `flex flex-col p-[10px] relative`,
+};
 
 const Chat = () => {
-  return (
-    <div className={style.main}>Chat</div>
-    
-  )
-}
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
 
-export default Chat
+  useEffect(() => {
+    const q = query(collection(db, "messages"), orderBy("timestamp"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <main className={style.main}>
+        {messages.length > 0 &&
+          messages.map((message) => {
+            return <Message key={message.id} message={message} />;
+          })}
+      </main>
+      {/* Enviar tu mensaje */}
+      <SendMessages />
+      <span ref={scroll}></span>
+    </>
+  );
+};
+
+export default Chat;
